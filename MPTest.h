665881e699803bb2ef46fd3a811ca9aa4aa98413
Barney Lund <Barney@Macintosh-2.local>
2014-05-26 16:27:52 -0600

    added test class

---
+++ MPTest.h
@@ -0,0 +1,13 @@
+//
+//  MPTest.h
+//  Git Test
+//
+//  Created by Barney Lund on 5/26/14.
+//  Copyright (c) 2014 Mana Mobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MPTest : NSObject
+
+@end
---
+++ MPTest.m
@@ -0,0 +1,13 @@
+//
+//  MPTest.m
+//  Git Test
+//
+//  Created by Barney Lund on 5/26/14.
+//  Copyright (c) 2014 Mana Mobile. All rights reserved.
+//
+
+#import "MPTest.h"
+
+@implementation MPTest
+
+@end
